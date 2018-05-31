88b9a71147c3c5d934ffffdecd58f61aae11d7b1
KabiOro <37700214+KabiOro@users.noreply.github.com>
2018-06-01 01:42:38 +0300

    Add files via upload

---
+++ subtract.cpp
@@ -0,0 +1,4 @@
+int subtract(int x, int y)
+{
+    return x - y;
+}
---
+++ subtract.h
@@ -0,0 +1,5 @@
+#ifndef SUBTRACT_H
+#define SUBTRACT_H
+int subtract(int x, int y); // function prototype for subtract.h 
+ 
+#endif
